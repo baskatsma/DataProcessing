51e2cb513b68f884aebebb75b709089c2f77e920
Bas Katsma <bas.katsma@student.uva.nl>
2018-02-22 14:25:55 +0100

    Store dates and precipitation in seperate arrays

--- Homework/week_3/barchart.js
+++ Homework/week_3/barchart.js
@@ -5,6 +5,20 @@
  *
  */
 
+// Load JSON dataFile
 d3.json("data.json", function(data) {
-  console.log(data);
+  
+  // Grab dates from JSON and store in separate array
+  var date = [];
+  for (i = 0; i < data.length; i++) {
+    date.push(data[i].date);
+  }
+  console.log(date);
+
+  // Grab precipitation from JSON and store in separate array
+  var precipitation = [];
+  for (i = 0; i < data.length; i++) {
+    precipitation.push(data[i].precipitation);
+  }
+  console.log(precipitation);
 });
