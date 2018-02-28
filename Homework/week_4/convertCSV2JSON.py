5da24304b0908702048bf8909d8a1b72c739533b
Bas Katsma <bas.katsma@student.uva.nl>
2018-02-28 14:50:44 +0100

    Fix inconsistent "" characters

--- Homework/week_4/convertCSV2JSON.py
+++ Homework/week_4/convertCSV2JSON.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     dataStorage = []
 
     # Read data file
-    with open('data_edited.csv', 'r') as dataFile:
-        dataReader = csv.DictReader(dataFile, JSONKeyNames, delimiter = ';')
+    with open("data_edited.csv", "r") as dataFile:
+        dataReader = csv.DictReader(dataFile, JSONKeyNames, delimiter = ";")
 
         # Iterate over each line
         for line in dataReader:
@@ -27,11 +27,11 @@ if __name__ == "__main__":
             # Match each value with its corresponding key
             data = OrderedDict()
             for key in JSONKeyNames:
-                data[key] = line[key].strip().replace('\ufeff', '')
+                data[key] = line[key].strip().replace("\ufeff", "")
             dataStorage.append(data)
 
     # Create new JSON file and use .dump to convert
-    with open('data_edited.json', 'w+') as JSONFile:
+    with open("data_edited.json", "w+") as JSONFile:
         json.dump(dataStorage, JSONFile, indent=4)
 
     # Close all files
