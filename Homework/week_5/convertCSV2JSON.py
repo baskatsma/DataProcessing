b12c9ca0f268005c89208c6ab6cb7d920bea9418
Bas Katsma <bas.katsma@student.uva.nl>
2018-03-01 17:26:09 +0100

    Upload data files (csv & JSON)

---
+++ Homework/week_5/convertCSV2JSON.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+#  Name: Bas Katsma
+#  Student 10787690
+#  Homework - Week 4
+#
+"""
+This script converts CSV to JSON.
+"""
+
+import csv
+import json
+from collections import OrderedDict
+
+if __name__ == "__main__":
+
+    # Initialize the keys
+    JSONKeyNames=["month", "avgWind", "highestWind", "lowestWind"]
+
+    # Create blank array to store the new data in
+    dataStorage = []
+
+    # Read data file
+    with open("data_edited_2017.csv", "r") as dataFile:
+        dataReader = csv.DictReader(dataFile, JSONKeyNames, delimiter=";")
+
+        # Iterate over each line
+        for line in dataReader:
+
+            # Match each value with its corresponding key
+            data = OrderedDict()
+            for key in JSONKeyNames:
+
+                # Remove whitespace and weird characters
+                data[key] = line[key].strip().replace("\ufeff", "")
+            dataStorage.append(data)
+
+    # Create new JSON file and use .dump to convert
+    with open("data_edited_2017.json", "w+") as JSONFile:
+        json.dump(dataStorage, JSONFile, indent=4)
+
+    # Close all files
+    dataFile.close()
+    JSONFile.close()
