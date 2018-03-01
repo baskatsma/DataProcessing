1a678ca32b0a329aa2b81eb4fd91f910142aa550
Bas Katsma <bas.katsma@student.uva.nl>
2018-03-01 18:21:07 +0100

    Fix spacing

--- Homework/week_5/d3line.js
+++ Homework/week_5/d3line.js
@@ -16,24 +16,24 @@ window.onload = function() {
         .defer(d3.json, 'data_edited_2017.json')
         .await(function(error, data2016, data2017) {
 
-          // Catch any errors
-          if (error) throw error;
+            // Catch any errors
+            if (error) throw error;
 
-          // Convert 2016 data strings to numbers
-          data2016.forEach(function(d) {
-              d.avgWind = Math.round(d.avgWind * 100) / 100;
-              d.highestWind = Math.round(d.highestWind * 100) / 100;
-              d.lowestWind = Math.round(d.lowestWind * 100) / 100;
-          });
+            // Convert 2016 data strings to numbers
+            data2016.forEach(function(d) {
+                d.avgWind = Math.round(d.avgWind * 100) / 100;
+                d.highestWind = Math.round(d.highestWind * 100) / 100;
+                d.lowestWind = Math.round(d.lowestWind * 100) / 100;
+            });
 
-          // Convert 2017 data strings to numbers
-          data2017.forEach(function(d) {
-              d.avgWind = Math.round(d.avgWind * 100) / 100;
-              d.highestWind = Math.round(d.highestWind * 100) / 100;
-              d.lowestWind = Math.round(d.lowestWind * 100) / 100;
-          });
+            // Convert 2017 data strings to numbers
+            data2017.forEach(function(d) {
+                d.avgWind = Math.round(d.avgWind * 100) / 100;
+                d.highestWind = Math.round(d.highestWind * 100) / 100;
+                d.lowestWind = Math.round(d.lowestWind * 100) / 100;
+            });
 
-          // Log upload output
-          console.log(data2016, data2017);
+            // Log upload output
+            console.log(data2016, data2017);
         });
 };
