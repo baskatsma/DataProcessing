030720ed052a2c4098e4e7b04eff059817d08e2c
Bas Katsma <bas.katsma@student.uva.nl>
2018-03-01 14:57:20 +0100

    Add window.onload function

--- Homework/week_4/Legend/legend.js
+++ Homework/week_4/Legend/legend.js
@@ -1,47 +1,51 @@
 /*
  *  legend.js
  *
- *  Bas Katsma
- *  10787690
+ *  Name: Bas Katsma
+ *  Student 10787690
+ *  Homework - Week 4
  *
  */
 
-var width = 360;
-var height = 360;
-var legendRectSize = 18;
-var legendSpacing = 4;
+window.onload = function() {
 
-var color = d3.scale.ordinal()
-  .domain(["Licht", "Medium", "Donker"])
-  .range(["#ffffb2","#fecc5c","#fd8d3c"])
+  var width = 360;
+  var height = 360;
+  var legendRectSize = 18;
+  var legendSpacing = 4;
 
-var svg = d3.select('.legend')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', 'translate(140, 40)');
+  var color = d3.scale.ordinal()
+    .domain(["Licht", "Medium", "Donker"])
+    .range(["#ffffb2","#fecc5c","#fd8d3c"])
 
-var legend = svg.selectAll('.legend')
-  .data(color.domain())
-  .enter()
-  .append('g')
-  .attr('class', 'legend')
-  .attr('transform', function(d, i) {
-    var height = legendRectSize + legendSpacing;
-    var offset =  height * color.domain().length / 2;
-    var horz = -2 * legendRectSize;
-    var vert = i * height - offset;
-    return 'translate(' + horz + ',' + vert + ')';
-  });
+  var svg = d3.select('.legend')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(140, 40)');
 
-legend.append('rect')
-  .attr('width', legendRectSize)
-  .attr('height', legendRectSize)
-  .style('fill', color)
-  .style('stroke', color);
+  var legend = svg.selectAll('.legend')
+    .data(color.domain())
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', function(d, i) {
+      var height = legendRectSize + legendSpacing;
+      var offset =  height * color.domain().length / 2;
+      var horz = -2 * legendRectSize;
+      var vert = i * height - offset;
+      return 'translate(' + horz + ',' + vert + ')';
+    });
 
-legend.append('text')
-  .attr('x', legendRectSize + legendSpacing)
-  .attr('y', legendRectSize - legendSpacing)
-  .text(function(d) { return d; });
+  legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+  legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
+};
