6c7e32e9d2b1fbbaf164c133f5b30ac68f12e94c
Bas Katsma <bas.katsma@student.uva.nl>
2018-03-01 18:17:29 +0100

    Use queue() to upload JSON files and convert strings to numbers

--- Homework/week_5/d3line.js
+++ Homework/week_5/d3line.js
@@ -10,14 +10,30 @@
 // Execute code after loading the DOM
 window.onload = function() {
 
-    // Initialize dimensions and margins
-    var margin = {top: 40, bottom: 15, left: 120, right: 100},
-        height = 650 - margin.top - margin.bottom,
-        width = 1100 - margin.left - margin.right;
-
-    // Load JSON data file
-    d3.queue()
+    // Load JSON data files
+    queue()
         .defer(d3.json, 'data_edited_2016.json')
         .defer(d3.json, 'data_edited_2017.json')
-        .await(function(error, file2016, file2017) { console.log(file2016, file2017); });
+        .await(function(error, data2016, data2017) {
+
+          // Catch any errors
+          if (error) throw error;
+
+          // Convert 2016 data strings to numbers
+          data2016.forEach(function(d) {
+              d.avgWind = Math.round(d.avgWind * 100) / 100;
+              d.highestWind = Math.round(d.highestWind * 100) / 100;
+              d.lowestWind = Math.round(d.lowestWind * 100) / 100;
+          });
+
+          // Convert 2017 data strings to numbers
+          data2017.forEach(function(d) {
+              d.avgWind = Math.round(d.avgWind * 100) / 100;
+              d.highestWind = Math.round(d.highestWind * 100) / 100;
+              d.lowestWind = Math.round(d.lowestWind * 100) / 100;
+          });
+
+          // Log upload output
+          console.log(data2016, data2017);
+        });
 };
