dac8a897365cfd948bdcb44b68627617f72a88f3
Bas Katsma <bas.katsma@student.uva.nl>
2018-03-01 15:46:27 +0100

    Add legend, title, and minor script updates

--- Homework/week_4/scatterplot.js
+++ Homework/week_4/scatterplot.js
@@ -10,19 +10,24 @@
 window.onload = function() {
 
     // Initialize dimensions and margins
-    var margin = {top: 40, bottom: 60, left: 120, right: 100},
+    var margin = {top: 40, bottom: 15, left: 120, right: 100},
         height = 650 - margin.top - margin.bottom,
         width = 1100 - margin.left - margin.right;
+    var legendRectSize = 20;
+    var legendSpacing = 5;
 
     // Apply dimensions to chart
     var svg = d3.select(".scatterplot")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Load JSON data file
-    d3.json("data_edited.json", function(data) {
+    d3.json("data_edited.json", function(error, data) {
+
+        // Catch any errors
+        if (error) throw error;
 
         // Convert string to a number with 2 decimals
         data.forEach(function(d) {
@@ -30,59 +35,74 @@ window.onload = function() {
           d.wellbeing = Math.round(d.wellbeing * 100) / 100;
         });
 
-        // this will be our colour scale. An Ordinal scale.
-        var colors = d3.scale.category10();
+        var color = d3.scale.category10();
 
-        // this sets the scale that we're using for the X axis.
-        // the domain define the min and max variables to show. In this case, it's the min and max prices of items.
-        // this is made a compact piece of code due to d3.extent which gives back the max and min of the price variable within the dataset
         var x = d3.scale.linear()
             .domain(d3.extent(data, function (d) {
             return d.lifeExpectancy;
         }))
-        // the range maps the domain to values from 0 to the width minus the left and right margins (used to space out the visualization)
             .range([0, width - margin.left - margin.right]);
 
-        // this does the same as for the y axis but maps from the rating variable to the height to 0.
         var y = d3.scale.linear()
             .domain(d3.extent(data, function (d) {
             return d.wellbeing;
         }))
-        // Note that height goes first due to the weird SVG coordinate system
             .range([height - margin.top - margin.bottom, 0]);
 
         var xAxis = d3.svg.axis()
             .scale(x)
-            .orient("bottom")
-            .tickPadding(2);
+            .orient("bottom");
 
         var yAxis = d3.svg.axis()
             .scale(y)
-            .orient("left")
-            .tickPadding(2);
+            .orient("left");
 
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + y.range()[0] + ")")
             .call(xAxis)
           .append("text")
-              .attr("class", "xtext")
-              .attr("x", width/2)
-              .attr("y", margin.bottom/1.5)
-              .attr("dy", ".71em")
-              .style("text-anchor", "end")
-              .text("Life expectancy (years)");
+            .attr("class", "xtext")
+            .attr("x", width / 2)
+            .attr("y", margin.bottom * 4)
+            .style("text-anchor", "end")
+            .text("Life Expectancy (years)");
 
         svg.append("g")
             .attr("class", "y axis")
             .call(yAxis)
           .append("text")
-              .attr("class", "ytext")
-              .attr("transform", "rotate(-90)")
-              .attr("y", -margin.left/2)
-              .attr("x", -height/4)
-              .style("text-anchor", "end")
-              .text("Wellbeing (1-10)");
+            .attr("class", "ytext")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -margin.left / 2)
+            .attr("x", -height / 4)
+            .style("text-anchor", "end")
+            .text("Wellbeing (1-10)");
 
+        svg.selectAll(".dot")
+            .data(data)
+          .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 3.5)
+            .attr("cx", function(d) { return x(d.lifeExpectancy); })
+            .attr("cy", function(d) { return y(d.wellbeing); })
+            .style("fill", function(d) { return color(d.region); });
+
+          var legend = svg.selectAll(".legend")
+              .data(color.domain())
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(725," + i * 30 + ")"; });
+
+          legend.append("rect")
+                .attr("width", legendRectSize)
+                .attr("height", legendRectSize)
+                .style("fill", color)
+                .style("stroke", color);
+
+          legend.append("text")
+                .attr("x", legendRectSize + (legendSpacing * 2))
+                .attr("y", legendRectSize - legendSpacing)
+                .text(function(d) { return d; });
         });
 };
